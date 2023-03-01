af4139bcbeeff18c88b7135e9e68e77eaf28d0f4
luis ownerPcDesktop <luis_dohko@hotmail.com>
2023-02-28 19:51:40 -0500

    copia de phpDoc
    se copio tal cual el archivo extencion.js y se copio y modifico el archivo app.js, se ordeno

--- extension.js
+++ extension.js
@@ -1,28 +1,62 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+const app = require('./src/app')
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "tsdocu" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
+	// let disposable = vscode.commands.registerCommand('tsdocu.helloWorld', function () {
+	// 	vscode.window.showInformationMessage('Hello World from tsdocu!');
+	// });
 	let disposable = vscode.commands.registerCommand('tsdocu.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+        var lang = vscode.window.activeTextEditor.document.languageId;
+        if (lang == "javascript" || lang == "typescript") {
+            var selection = vscode.window.activeTextEditor.selection;
+            var startLine = selection.start.line;
+            var selectedText = vscode.window.activeTextEditor.document.lineAt(startLine).text;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tsdocu!');
-	});
+            var textToInsert = '';
+            if (/function\s+([\w_-]+)/.exec(selectedText) != null) {
+                textToInsert = app.comment(selectedText);
+            }  else {
+                vscode.window.showInformationMessage('Please select a javascript signature or typescript');
+                return;
+            }
+
+            vscode.window.activeTextEditor.edit(function (editBuilder) {
+                startLine--;
+                if (startLine < 0) {
+                    startLine = 0;
+                    textToInsert = textToInsert + '\n';
+                }
+
+                var lastCharIndex = vscode.window.activeTextEditor.document.lineAt(startLine).text.length;
+                var pos;
+
+                if ((lastCharIndex > 0) && (startLine != 0)) {
+                    pos = new vscode.Position(startLine, lastCharIndex);
+                } else {
+                    pos = new vscode.Position(startLine, 0);
+                }
+
+                textToInsert = '\n' + textToInsert;
+
+                var line = vscode.window.activeTextEditor.document.lineAt(selection.start.line).text;
+                var firstNonWhiteSpace = vscode.window.activeTextEditor.document.lineAt(selection.start.line).firstNonWhitespaceCharacterIndex;
+                var stringToIndent = '';
+                for (var i = 0; i < firstNonWhiteSpace; i++) {
+                    if (line.charAt(i) == '\t') {
+                        stringToIndent = stringToIndent + '\t';
+                    } else if (line.charAt(i) == ' ') {
+                        stringToIndent = stringToIndent + ' ';
+                    }
+                }
+                textToInsert = textToInsert.replace(/^/gm, stringToIndent);
+                editBuilder.insert(pos, textToInsert);
+            })
+        }
+    });
 
 	context.subscriptions.push(disposable);
 }
